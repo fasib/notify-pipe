e48bfa37a3196e2170056703f39f10a5f4c3be51
Alexander Treptau <alex@treptau.eu>
2015-09-15 13:08:17 +0200

    updated readme text and added the license/description to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setup(
     author='Alexander Treptau',
     author_email='alex@treptau.eu',
     url='https://github.com/fasib/notify-pipe',
+    description='like notify-send just for pipes',
+    long_description='notify-pipe allows you to pipe the output from any command to desktop notifications',
+    license='MIT',
     py_modules=['notify_pipe',],
     entry_points={
         'console_scripts': [
